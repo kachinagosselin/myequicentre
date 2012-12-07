d1c3df795e8c206302b47cc41f1c0728d33ebfc0
Kachina Gosselin <kachina@alum.mit.edu>
2012-12-06 20:20:15 -0500

    Implemented rough version of drafts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -63,6 +63,17 @@ class MessagesController < ApplicationController
         end
   end
     
+  def drafts
+      @user = User.find(params[:user_id])
+      @message = @user.messages.build(params[:message])
+      @message.sent = false
+      @message.folder = "drafts"  
+      respond_to do |format|
+            format.html # index.html.erb
+            format.json { render json: user_messages_path(@user) }
+      end
+  end
+  
   # GET user/:user_id/messages/archive
   # GET user/:user_id/messages/archive/1
   def archive
@@ -74,7 +85,7 @@ class MessagesController < ApplicationController
       @message.folder = "archive"  
       respond_to do |format|
           if @message.save
-              format.html { redirect_to user_messages_archive_path(@user), notice: 'Message was successfully archived.' }
+              format.html { redirect_to :back, notice: 'Message was successfully archived.' }
               format.json { head :no_content }
               else
               #format.html { render action: "new" }
@@ -96,7 +107,7 @@ class MessagesController < ApplicationController
       @message.folder = "trash"  
       respond_to do |format|
           if @message.save
-              format.html { redirect_to user_messages_trash_path(@user), notice: 'Message was successfully moved to trash.' }
+              format.html { redirect_to :back, notice: 'Message was successfully moved to trash.' }
               format.json { head :no_content }
               else
               #format.html { render action: "new" }
@@ -110,15 +121,30 @@ class MessagesController < ApplicationController
   # POST user/:user_id/messages
   # POST user/:user_id/messages.json
   def create
+      if params[:save_button]
+              @user = User.find(params[:user_id])
+      @message = @user.messages.build(params[:message])
+      @message.folder = "drafts"
+        respond_to do |format|
+          if @message.save
+              format.html { redirect_to user_messages_drafts_path(@user), notice: 'Message was successfully saved.' }
+              format.json { head :no_content }
+              else
+              #format.html { render action: "new" }
+              format.html { redirect_to new_user_messages_path(@user), alert: 'Message was unsuccessfully saved.' }
+              format.json { render json: @message.errors, status: :unprocessable_entity}
+          end
+        end
+      else 
       @sender = User.find(params[:user_id])
       @message_sent = @sender.messages.build(params[:message])
       @message_sent.folder = "sent"
       @recipient = User.find(@message_sent.to_user_id)
       @message_recieved = @recipient.messages.build(params[:message])
       @message_recieved.folder = "inbox"
-      
     respond_to do |format|
       if @message_sent.save && @message_recieved.save
+        @message_sent.sent = true
         format.html { redirect_to user_messages_path(@sender), notice: 'Message was successfully created.' }
         #format.json { render json: @message, status: :created, location: @message }
         format.json { head :no_content }
@@ -128,11 +154,33 @@ class MessagesController < ApplicationController
         format.json { render json: @message_sent.errors, status: :unprocessable_entity}
       end
     end
+    end
   end
-
+    
   # PUT user/:user_id/messages/1
   # PUT user/:user_id/messages/1.json
   def update
+    if params[:send_button]
+      @sender = User.find(params[:user_id])
+      @message_sent = @sender.messages.find(params[:id])
+      @recipient = User.find(@message_sent.to_user_id)
+      @message_recieved = @recipient.messages.build(params[:message])
+      @message_sent.folder = "sent"
+      @message_recieved.folder = "inbox"
+        respond_to do |format|
+        if @message_sent.save && @message_recieved.save
+        @message_sent.sent = true
+        format.html { redirect_to user_messages_path(@sender), notice: 'Message was successfully created.' }
+        #format.json { render json: @message, status: :created, location: @message }
+        format.json { head :no_content }
+        else
+        #format.html { render action: "new" }
+        format.html { redirect_to user_messages_path(@sender), alert: 'Message was unsuccessfully created.' }
+        format.json { render json: @message_sent.errors, status: :unprocessable_entity}
+        end
+    end
+      
+    else
       @user = User.find(params[:user_id])
       @message = @user.messages.find(params[:id])
       
@@ -144,6 +192,7 @@ class MessagesController < ApplicationController
         format.html { render action: "edit" }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
+    end
     end
   end
 
