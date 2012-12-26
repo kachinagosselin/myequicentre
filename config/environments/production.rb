9b0b9cb7a540b728af314c8b9c72ca502d308dcd
Kachina Gosselin <kachina@alum.mit.edu>
2012-12-26 16:42:20 -0500

    Adding google analytics properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,20 +89,7 @@ Railsapp::Application.configure do
     }  
 
 
-    # replace this with your tracker code
-    <script type="text/javascript">
-    
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-31693337-1']);
-    _gaq.push(['_trackPageview']);
-    
-    (function() {
-     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-     })();
-    
-    </script>  
+    # replace this with your tracker code  
     GA.tracker = "UA-31693337-1"
     
   # Log the query plan for queries taking more than this (works
